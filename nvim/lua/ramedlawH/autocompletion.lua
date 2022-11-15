cf8e54cc1239a89bcea488054980c5cb50da88a4
Waldemar Hecht <ramedlawH@users.noreply.github.com>
2022-11-15 10:31:40 +0100

    Fix deprecated require for nvm lsp

--- nvim/lua/ramedlawH/autocompletion.lua
+++ nvim/lua/ramedlawH/autocompletion.lua
@@ -65,7 +65,7 @@ cmp.setup.cmdline(':', {
 })
 
 -- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- TS Server
 require('lspconfig')['tsserver'].setup {
